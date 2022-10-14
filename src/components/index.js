8c54064d7953ff486fd13d37befcc2acb65783db
leonardo <leonardofarena@gmail.com>
2022-10-14 18:56:50 -0300

    n

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export * from './home/HomeContent';
 export * from './services/Services';
-export * from './navbar/Navbar';
+export * from './navbar/NavBar';
 export * from './footer/Footer';
 export * from './banner/Banner';
 
