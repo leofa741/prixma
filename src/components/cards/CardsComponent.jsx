5dd9ac7a516a7a3f0b73c4dad1a62ccab08390d8
leonardo <leonardofarena@gmail.com>
2022-10-15 17:43:16 -0300

    hh

--- src/components/cards/CardsComponent.jsx
+++ src/components/cards/CardsComponent.jsx
@@ -5,139 +5,162 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import Grid from '@mui/material/Grid';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+
 import './cardscomponent.css'
 
+
+
 export default function ActionAreaCard() {
-  return (
- <Grid 
-    container   
-    spacing={2}
-      direction="row"
-        justifyContent="center"
-        alignItems="center"
-        className="cards-container">
+    return (
+        <Grid
+            container
+            spacing={2}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            className="cards-container">
 
-    <Grid item xs={12} sm={6} md={4} lg={3} 
-    >
-        <Card sx={{ maxWidth: 345 }}
-        className="card"
-        >
-             <CardActionArea
-               
-             >
-                      <CardMedia
-                          className="card-img">
-                          <btn className=" card-img-btn  btn  btn-block">Diseño Web</btn>
-                          <CardContent
-                              className="card-body">
-                              <Typography gutterBottom variant="h2" component="div">
-                                  Lizard
-                              </Typography>
-                              <Typography
-                                    variant="body2"
-                                    className="card-text"  
-                                    sx={{ fontSize: 10 }}
+            <Grid item xs={12} sm={6} md={4} lg={3}
+            >
+                <Card sx={{ maxWidth: 345 }}
+                    className="card"
+                >
+                    <CardActionArea
+                    >
+                        <CardMedia
+                            className="card-img">
 
-                                  >
-                          
-                             
+                            <CardContent
+                                className="card-body">
+                                <Typography gutterBottom variant="h2" component="div"
+                                    sx={{ color: 'black' }}
+                                    fontWeight="bold"
+                                    fontSize="1.3rem"
+                                >
+                                    Involucrar a clientes y usuarios
+                                </Typography>
+                                <GroupAddIcon
+                                    sx={{
+                                        color: 'black',
+                                        fontWeight: 'bold',
+                                        fontSize: '3.3rem'
+                                    }}   />
+                                <Typography variant="body2"
+                                    className="card-text">
+                                    <p>Enfocamos nuestros esfuerzos en involucrar a nuestros clientes y usuarios en el desarrollo de nuestros proyectos, para que puedan aportar sus ideas y sugerencias, y así poder ofrecerles un producto final que cumpla con sus expectativas.</p>
+                                </Typography>
+                            </CardContent>
+                        </CardMedia>
+                    </CardActionArea>
+                </Card>
+            </Grid>
 
-                         
+            <Grid item xs={12} sm={6} md={4} lg={3}
+            >
+                <Card sx={{ maxWidth: 345 }}
+                    className="card"
+                >
+                    <CardActionArea >
+                        <CardMedia
+                            className="card-img1">
+                            <CardContent
+                                className="card-body">
+                                <Typography gutterBottom variant="h2" component="div"
+                                    sx={{ color: 'black' }}
+                                    fontWeight="bold"
+                                    fontSize="1.3rem"
+                                >
+                                    Negocio y Producto Innovadores
+                                </Typography>
+                                <TipsAndUpdatesIcon
+                                    sx={{
+                                        color: 'yellow',
+                                        fontWeight: 'bold',
+                                        fontSize: '3.3rem'
+                                    }}  />
+                                <Typography variant="body2"
+                                    className="card-text">
+                                    Los productos innovadores son productos diseñados para estimular las ventas y a la vez generar una mayor tasa de recompra.
 
-                                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                  across all continents except Antarctica
-                              </Typography>
-                          </CardContent>
-                      </CardMedia>
-                  </CardActionArea>
-        </Card>
+                                </Typography>
+                            </CardContent>
+                        </CardMedia>
+                    </CardActionArea>
+                </Card>
+            </Grid>
 
+            <Grid item xs={12} sm={6} md={4} lg={3}
+            >
+                <Card sx={{ maxWidth: 345 }}
+                    className="card"  >
+                    <CardActionArea >
+                        <CardMedia
+                            className="card-img2">
+                            <CardContent
+                                className="card-body">
+                                <Typography gutterBottom variant="h2" component="div"
+                                    sx={{ color: 'black' }}
+                                    fontWeight="bold"
+                                    fontSize="1.3rem"
+                                >
+                                    Optimización de aplicaciones e infraestructura de operaciones
+                                </Typography>
+
+                                <MiscellaneousServicesIcon
+                                    sx={{
+                                        color: 'black',
+                                        fontWeight: 'bold',
+                                        fontSize: '3.3rem'
+                                    }}
+                                />
+                                <Typography variant="body2"
+                                    className="card-text">
+                                </Typography>
+                            </CardContent>
+                        </CardMedia>
+                    </CardActionArea>
+                </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={4} lg={3}
+            >
+                <Card sx={{ maxWidth: 345 }}
+                    className="card"
+                >
+                    <CardActionArea
+                    >
+                        <CardMedia
+                            className="card-img3">
+
+                            <CardContent
+                                className="card-body">
+                                <Typography gutterBottom variant="h2" component="div"
+                                    sx={{ color: 'black' }}
+                                    fontWeight="bold"
+                                    fontSize="1.3rem"
+                                >
+                                    Empoderar a los empleados
+                                </Typography>
+                                <PersonAddAltIcon
+                                    sx={{
+                                        color: 'black',
+                                        fontWeight: 'bold',
+                                        fontSize: '3.3rem'
+                                    }}
+                                />
+                                <Typography variant="body2"
+                                    className="card-text">
+                                </Typography>
+                            </CardContent>
+                        </CardMedia>
+                    </CardActionArea>
+                </Card>
+            </Grid>
         </Grid>
 
-        <Grid item xs={12} sm={6} md={4} lg={3} 
-    >
-        <Card sx={{ maxWidth: 345 }}
-        className="card"
-        >
-             <CardActionArea>
-                      <CardMedia
-                          className="card-img">
-                          <btn className=" card-img-btn  btn  btn-block">Diseño Web</btn>
-                          <CardContent
-                              className="card-body">
-                              <Typography gutterBottom variant="h2" component="div">
-                                  Lizard
-                              </Typography>
-                              <Typography variant="body2"
-                                  className="card-text">
-
-                                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                  across all continents except Antarctica
-                              </Typography>
-                          </CardContent>
-                      </CardMedia>
-                  </CardActionArea>
-        </Card>
-
-        </Grid>
- 
-        <Grid item xs={12} sm={6} md={4} lg={3} 
-    >
-        <Card sx={{ maxWidth: 345 }}
-        className="card"
-        >
-             <CardActionArea>
-                      <CardMedia
-                          className="card-img">
-                          <btn className=" card-img-btn  btn  btn-block">Diseño Web</btn>
-                          <CardContent
-                              className="card-body">
-                              <Typography gutterBottom variant="h2" component="div">
-                                  Lizard
-                              </Typography>
-                              <Typography variant="body2"
-                                  className="card-text">
-
-                                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                  across all continents except Antarctica
-                              </Typography>
-                          </CardContent>
-                      </CardMedia>
-                  </CardActionArea>
-        </Card>
-
-        </Grid>
-
-        <Grid item xs={12} sm={6} md={4} lg={3} 
-    >
-        <Card sx={{ maxWidth: 345 }}
-        className="card"
-        >
-             <CardActionArea>
-                      <CardMedia
-                          className="card-img">
-                          <btn className=" card-img-btn  btn  btn-block">Diseño Web</btn>
-                          <CardContent
-                              className="card-body">
-                              <Typography gutterBottom variant="h2" component="div">
-                                  Lizard
-                              </Typography>
-                              <Typography variant="body2"
-                                  className="card-text">
-
-                                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                  across all continents except Antarctica
-                              </Typography>
-                          </CardContent>
-                      </CardMedia>
-                  </CardActionArea>
-        </Card>
-
-        </Grid>
-
-        
-
-    </Grid>
-
-  );
+    );
 }
