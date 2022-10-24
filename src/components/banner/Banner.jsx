4c031cd8a623ef6e17a150f54a1f3e12ca8464c4
leonardo <leonardofarena@gmail.com>
2022-10-24 13:52:47 -0300

    nnn

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -10,7 +10,8 @@ export const Banner = () => {
     <div className="banner-content">
     <h1 className="banner-title">Puentes Digitales</h1>
     <p className="banner-text">Somos una empresa de desarrollo de software, especializada en la creación de aplicaciones web y móviles.</p>
-    <a href="#contact" className="banner-btn">Saber más...</a>
+    <a href="https://wa.me/+5491136038167?text=Hola%20me%20gustaria%20saber%20mas%20sobre%20los%20concesionarios%20online" className="banner-btn">Contactanos</a>
+               
     </div>
     </div>
 
