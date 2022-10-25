d7fc5d47adbfe04e7be7b0063fd240d4259dafca
leonardo <leonardofarena@gmail.com>
2022-10-25 18:26:56 -0300

    nnn

--- src/components/magnamentsistem/MagnamentSystemComponent.jsx
+++ src/components/magnamentsistem/MagnamentSystemComponent.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Milestone } from '../milestone/Milestone'
 import { Portfolio } from '../servicedisweb/Portfolio'
+import './magnament.css'
+import { MagnamentServices } from './MagnamentServices'
 
 
 
@@ -11,6 +13,33 @@ export const MagnamentSystemComponent = () => {
         title="Sistema de Gestión"
         descripcion="Sistema de gestión para distribuidores de productos de consumo masivo, con el cual podrás administrar tu inventario de manera eficiente y segura de forma online ."
         />
+
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                  <div className="card-img-magnament">
+                  <div class="contenedor-texto">
+                    <p>
+                      <h3>¿Qué es un sistema de gestión?</h3>
+                      <br/>
+                      Un sistema de gestión es un conjunto de procesos que se encargan de administrar los recursos de una empresa, con el fin de lograr los objetivos de la misma.
+                      .</p>
+                    </div>
+                  </div>
+                   
+                </div>
+                <div className="col-md-6">
+                    <h2 className="text-center">Sistema de Gestión</h2>
+                    <p className="text-center">Sistema de gestión para distribuidores de productos de consumo masivo, con el cual podrás administrar tu inventario de manera eficiente y segura de forma online .</p>
+                  
+               
+                </div>
+                
+            </div>
+        </div>
+
+
+<MagnamentServices />
 
 
 
