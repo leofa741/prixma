b23474e75d41dd5f6b2b96ee03e46c2c04aea677
leonardo <leonardofarena@gmail.com>
2022-10-27 11:37:09 -0300

    nn

--- src/components/design/ServicesDesign.jsx
+++ src/components/design/ServicesDesign.jsx
@@ -5,33 +5,10 @@ export const ServicesDesign = () => {
  <>
  <section id="services" className="services">
 
-    <div className="row text-center">
-      <h1 className="display-3 fw-bold">Our Services</h1>
-      <div className="heading-line mb-1"></div>
-    </div>
-
-    <div className="row pt-2 pb-2 mt-0 mb-3">
-      <div className="col-md-6 border-right">
-        <div className="bg-white p-3">
-          <h2 className="fw-bold text-capitalize text-center">
-            Our Services Range From Initial Design To Deployment Anywhere Anytime
-          </h2>
-        </div>
-      </div>
-      <div className="col-md-6">
-        <div className="bg-white p-4 text-start">
-          <p className="fw-light">
-            Lorem ipsum dolor sit amet consectetur architecto magni, 
-            dicta maxime laborum temporibus dolorem esse doloremque illo quas nisi enim molestias. 
-            Tempore ducimus molestiae in dolore enim.
-          </p>
-        </div>
-      </div>
-    </div>
+    
 
 
-
-
+  <div className="container">
 
     <div className="row">
       <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 services mt-4">
@@ -98,7 +75,7 @@ export const ServicesDesign = () => {
         </div>
       </div>
     </div>
-
+  </div>
 </section>
  </>
   )
