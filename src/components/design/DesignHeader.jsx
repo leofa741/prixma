171fc41426752eac76eecea1449e82ad2e93bf88
leonardo <leonardofarena@gmail.com>
2022-10-27 13:47:01 -0300

    nnn

--- src/components/design/DesignHeader.jsx
+++ src/components/design/DesignHeader.jsx
@@ -15,7 +15,11 @@ export const DesignHeader = () => {
 
           </span>
           <span className="display-2--description lh-base">
-            dsgfdsgdsgdfsgdfggfd                  
+          Desarrollamos la imagen de tu empresa, diseño gráfico a medida
+Tenemos 20 años de experiencia de Diseño Gráfico.
+Contamos con profesionales expertos en Diseño Gráfico.
+
+                  
 
 
           </span>
--- src/components/design/ServicesHeader.jsx
+++ src/components/design/ServicesHeader.jsx
@@ -5,28 +5,9 @@ export const ServicesHeader = () => {
   <>
     <div className="container">
   <div className=" text-center">
-      <h1 className="display-3 fw-bold">Our Services</h1>
+      <h1 className="display-3 fw-bold">Servicio</h1>
       <div className="heading-line mb-1"></div>
-    </div>
-
-    <div className=" pt-2 pb-2 mt-0 mb-3">
-      <div className="col-md-6 border-right">
-        <div className="bg-white p-3">
-          <h2 className="fw-bold text-capitalize text-center">
-            Our Services Range From Initial Design To Deployment Anywhere Anytime
-          </h2>
-        </div>
-      </div>
-      <div className="col-md-6">
-        <div className="bg-white p-4 text-start">
-          <p className="fw-light">
-            Lorem ipsum dolor sit amet consectetur architecto magni, 
-            dicta maxime laborum temporibus dolorem esse doloremque illo quas nisi enim molestias. 
-            Tempore ducimus molestiae in dolore enim.
-          </p>
-        </div>
-      </div>
-    </div>
+    </div>  
 
  </div>
 
