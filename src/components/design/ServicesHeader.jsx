39e8fc0289aaca370fa92747479a9138c2b2ffd2
leonardo <leonardofarena@gmail.com>
2022-10-27 19:44:48 -0300

    nnn

--- src/components/design/ServicesHeader.jsx
+++ src/components/design/ServicesHeader.jsx
@@ -5,7 +5,7 @@ export const ServicesHeader = () => {
   <>
     <div className="container">
         <div className=" text-center">
-      <h1 className="display-3 fw-bold">Servicio</h1>
+      <h1 className="display-3 fw-bold">Servicios</h1>
       <div className="heading-line mb-1"></div>
       </div>  
      </div>
