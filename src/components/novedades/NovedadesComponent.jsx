6bc04ef82899cfb103770c045ca065ca1512bb77
leonardo <leonardofarena@gmail.com>
2022-10-28 08:57:22 -0300

    bbb

--- src/components/novedades/NovedadesComponent.jsx
+++ src/components/novedades/NovedadesComponent.jsx
@@ -42,32 +42,6 @@ export const NovedadesComponent = () => {
 
 
 
-        <div className="novedades">
-
-        <div className="novedades__container">
-          <div className="novedades__container__title1">
-            <h2>App Android IOS</h2>
-          </div>
-          <div className="novedades__container__cards">
-            <div className="novedades__container__cards__card">
-              <div className="novedades__container__cards__card__img1">
-                <div className="novedades__container__cards__card__img__img1">
-                </div>
-              </div>
-              <div className="novedades__container__cards__card__text1">
-                <h3>
-                  E-commerce
-                </h3>
-                <p>
-                  Desde cualquier lugar, con cualquier dispositivo y a toda hora se pueden visitar las góndolas virtuales y elegir el producto que necesita agregandolo a su carrito de compras personalizado.
-                  El proveedor puede conocer las ventas en tiempo real y tener reportes.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
     
 
 
