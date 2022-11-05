8c68bb3f0a0c12fa5930b97ee2ba2e3aaa813982
leonardo arena <leonardofarena@gmail.com>
2022-11-05 11:34:20 -0300

    nnn

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -35,7 +35,7 @@ export const NavBar = () => {
     
  <nav className="navbar navbar-expand-lg navbar-light bg-white sticky-top">
     <div className="container">
-         <Link  className="navbar-brand" to='/' >PuentesDigitales</Link> <img src="https://github.com/leofa741/prixma/blob/master/src/assets/img/icon6.png?raw=true" alt="logo" className="logo" />
+         <Link  className="navbar-brand" to='/' >PuentesDigitales</Link> <img src="https://github.com/leofa741/prixma/blob/master/src/assets/img/favicon.ico?raw=true" alt="logo" className="logo" />
 
        
 
