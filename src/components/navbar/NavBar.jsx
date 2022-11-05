5b774844cb09bcf0f7c2e44468b304773fe255bf
leonardo arena <leonardofarena@gmail.com>
2022-11-05 11:21:47 -0300

    nnn

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -35,7 +35,8 @@ export const NavBar = () => {
     
  <nav className="navbar navbar-expand-lg navbar-light bg-white sticky-top">
     <div className="container">
-         <Link  className="navbar-brand" to='/' >PuentesDigitales</Link> 
+         <Link  className="navbar-brand" to='/' >PuentesDigitales</Link> <img src="https://github.com/leofa741/prixma/blob/master/src/assets/img/icon6.png?raw=true" alt="logo" className="logo" />
+
        
 
              <span className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
