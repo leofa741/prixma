83d30ea0eeed54ef18ca5652d1c137b8f54d82b5
leonardo arena <leonardofarena@gmail.com>
2022-12-01 16:00:23 -0300

    nnnn

--- src/components/shiftmagnamentsystem/ShiftMagnamentSystemCompnent.jsx
+++ src/components/shiftmagnamentsystem/ShiftMagnamentSystemCompnent.jsx
@@ -102,12 +102,12 @@ export const ShiftMagnamentSystemCompnent =(props) => {
                     </div>
                     <div className="d-flex align-items-center mb-3">
                         <h5 className="border-end pe-3 me-3 mb-0">VALOR</h5>
-                        <h2 className="text-primary fw-bold mb-0" data-toggle="counter-up">$ 90.000</h2>
+                        <h2 className="text-primary fw-bold mb-0" data-toggle="counter-up">$ 120.000</h2>
                     </div>
                     <p className="mb-4"> <i className="far fa-check-circle text-primary me-3"></i>Puedes mejorar la atención a tus clientes gracias a nuestro software de gestión de colas</p>
                     <div className="d-flex align-items-center mb-3">
                         <h5 className="border-end pe-3 me-3 mb-0">ABONO MENSUAL</h5>
-                        <h2 className="text-primary fw-bold mb-0" data-toggle="counter-up">$ 3.500</h2>
+                        <h2 className="text-primary fw-bold mb-0" data-toggle="counter-up">$ 5.500</h2>
                     </div>
                     <p className="mb-0">¿Qué incluye el Abono mensual?</p>
                     <p className="mb-3"><i className="far fa-check-circle text-primary me-3"></i>Hosting | Alojamiento de su soft en Internet.</p>
