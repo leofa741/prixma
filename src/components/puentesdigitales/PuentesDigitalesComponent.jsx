54556670b451bc62134ca5577306eac55f781aae
leonardo arena <leonardofarena@gmail.com>
2022-12-06 08:41:20 -0300

    nnnn

--- src/components/puentesdigitales/PuentesDigitalesComponent.jsx
+++ src/components/puentesdigitales/PuentesDigitalesComponent.jsx
@@ -5,14 +5,10 @@ import productos from "../../database/datadb";
 
 
 
-
-
-
 export const PuentesDigitalesComponent =(props) => {
   const [items, setItems] = useState([]);
 
-  useEffect(() => {
-      
+  useEffect(() => {      
         customFetch(2000, productos).then((data) => setItems(data));
        }, []);
 
@@ -26,8 +22,7 @@ export const PuentesDigitalesComponent =(props) => {
                       <h4 className="text-center"> {props.title}</h4>                 
                       <p className="text-center">Somos los Puentes Digitales de tus proyectos, con más de 10 años de experiencia en el mercado ofreciendo soluciones tecnológicas a medida para empresas de todos los tamaños. Nuestro objetivo es ayudar a nuestros clientes a alcanzar sus objetivos de negocio, a través de la implementación de soluciones tecnológicas que les permitan mejorar su productividad y competitividad.</p>
             
-              </div>
-      
+              </div>      
             
               <Box sx={{ width: "100%"      }}>
                 <h5>Cargando... </h5> 
@@ -62,7 +57,7 @@ export const PuentesDigitalesComponent =(props) => {
                                  
                 </p>
                 <p className="text-center">
-                  Operamos en la zona sur de Buenos Aires, en la ciudad de La Plata, y en la ciudad de Mar del Plata.
+                  Operamos en la zona sur de Buenos Aires.
                   Nuestro objetivo es ayudar a nuestros clientes a alcanzar sus objetivos de negocio,
                   a través de la implementación de soluciones tecnológicas que les permitan mejorar su productividad y competitividad.
                 </p>
