35e2ca6f2bdd4bb6acc5031ec3754f65175a2ca1
leonardo arena <leonardofarena@gmail.com>
2022-12-07 09:48:13 -0300

    mmm

--- src/components/novedades/NovedadesComponent.jsx
+++ src/components/novedades/NovedadesComponent.jsx
@@ -27,7 +27,7 @@ export const NovedadesComponent =(props) => {
               </div>
       
             
-              <Box sx={{ width: "100%"      }}>
+              <Box sx={{ width: "100%" }}>
                 <h5>Cargando... </h5> 
                 <LinearProgress />
                 <br></br>
@@ -56,11 +56,23 @@ export const NovedadesComponent =(props) => {
 
               <div className="novedades__container__cards__card__text">
                 <h3>
-                  diseño de ecommerce
+                 Porque tener un sitio web es importante para tu negocio
                 </h3>
                 <p>
-                  Si estás buscando vender por internet, o transformar tu página web en una verdadera herramienta de ventas.
-                  te ayudamos a diseñar tu tienda online.,
+                  En la actualidad, tener un sitio web es una necesidad para cualquier negocio, ya que es la mejor forma de dar a conocer tu empresa, tus productos y servicios, y de llegar a más clientes potenciales.
+
+                  La mayoría de las personas que buscan algún producto o servicio, lo hacen a través de internet, por lo que si no tienes un sitio web, estás perdiendo la oportunidad de llegar a muchos clientes potenciales.
+
+                  Además, tener un sitio web te permite tener un mayor control sobre tu negocio, ya que puedes actualizar la información de tu empresa, tus productos y servicios, y tus redes sociales, de manera rápida y sencilla.
+
+                  Por otro lado, tener un sitio web te permite tener una presencia en internet las 24 horas del día, los 7 días de la semana, lo que te permite llegar a más clientes potenciales, y aumentar tus ventas.
+
+                  Contactanos y te ayudaremos a crear un sitio web para tu negocio.
+
+
+
+              
+               
                 </p>
               </div>
             </div>
