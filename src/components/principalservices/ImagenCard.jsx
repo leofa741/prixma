f4d155b29ff1ccef19cba1c4f4edccecc774926c
leonardo arena <leonardofarena@gmail.com>
2023-11-13 13:57:03 -0300

    b

--- src/components/principalservices/ImagenCard.jsx
+++ src/components/principalservices/ImagenCard.jsx
@@ -3,42 +3,42 @@ import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { customFetch } from '../../database/customfech';
+import './contentservices.css'
 
-export const ImagenCard = ({url}) => {
 
-    const [items, setItems] = useState([]);
+export const ImagenCard = ({ url }) => {
 
-    useEffect(() => {
-        
-          customFetch(2000, 'imagen').then((data) => setItems(data));
-         }, []);
+  const [items, setItems] = useState([]);
 
-        if (items.length === 0) {
-            return (                     
-              
-                <Box sx={{ width: "100%"      }}>
-                  <h5
-                  style={{
-                    color: "black",
-                    textAlign: "center",
-                    fontWeight: "bold",
-                    fontSize: "0.6rem",
-                    fontFamily: "Roboto",
-                    textTransform: "uppercase",
-                    letterSpacing: "0.2rem",
-                    textShadow: '4px 4px 9px #000000',
-                    }}
-                  >Cargando...   <LinearProgress /></h5>                 
-                
-                </Box>            
-            );
-        }
-        
-        else{
-    
-  return (
-    <img className="img-fluid animacion-block1" src={url} alt="alternative" />
-  )
-}
+  useEffect(() => {
+    customFetch(2000, 'imagen').then((data) => setItems(data));
+  }, []);
+
+  if (items.length === 0) {
+    return (
+      <Box sx={{ width: "100%" }}>
+        <h5
+          style={{
+            color: "black",
+            textAlign: "center",
+            fontWeight: "bold",
+            fontSize: "0.6rem",
+            fontFamily: "Roboto",
+            textTransform: "uppercase",
+            letterSpacing: "0.2rem",
+            textShadow: '4px 4px 9px #000000',
+          }}
+        >Cargando...   <LinearProgress /></h5>
+
+      </Box>
+    );
+  }
+
+  else {
+
+    return (
+      <img className="img-fluid animacion-block1" src={url} alt="alternative" />
+    )
+  }
 
 }
