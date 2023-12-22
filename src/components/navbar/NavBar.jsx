ff75d99e065a87becc0a69aa40b71001b5901e8a
leonardofarena <leonardofarena@gmail.com>
2023-12-22 15:15:14 +0000

    nnn

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -43,9 +43,7 @@ export const NavBar = () => {
             textCombineUpright: "none",
             textOrientationVertical: "mixed",
             marginTop: "-10px",
-          }}
-          
-          >PuentesDigitales</Link> 
+          }}>PuentesDigitales</Link> 
        <a className='textlogo'
             style={{
                 color: "white",
@@ -75,63 +73,49 @@ export const NavBar = () => {
                 textCombineUpright: "none",
                 textOrientationVertical: "mixed",
                 marginTop: "30px",
-            }}
-       >deploying ideas  </a>       
+            }}>deploying ideas  </a>       
 
              <span className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><MenuIcon 
                 sx={{ color: 'white',fontSize: 30,'&:hover': {color: 'white',fontSize: 35, },}}/></span>            
             
-        
-        <div className="collapse navbar-collapse" id="navbarNav" >
+            {/* data-bs-toggle="collapse" data-bs-target="#navbarNav"  */}
+        <div className="collapse navbar-collapse" id="navbarNav"  >
             <ul className="navbar-nav ms-auto">
-
-            {/* <li className="nav-item" >
-                <NavLink  className="nav-link" to="" 
-                 data-bs-toggle="collapse" data-bs-target="#navbarNav" 
-                onClick={() => confetti({
-                    particleCount: 600,
-                    spread: 170,
-                    origin: { y: 0.6 },
-                    colors: [  '#26ccff', '#000000','#ffffff',]
-                  })}
-                >   campeones del mundo    </NavLink>           
-                </li> */}
-
+         
 
                 <li className="nav-item" >
                 <NavLink  to="/home"  className={ ({ isActive }) => `nav-link   ${isActive ? 'active':''}` } >  
-                  <span className='active1' data-bs-toggle="collapse" data-bs-target="#navbarNav" 
-                  >  Home  </span>       </NavLink>           
+                  <span className='active1' >Home </span></NavLink>           
                 </li>
 
                 <div className="dropdown">
                 <li className="nav-item">
-                    <NavLink  className={ ({ isActive }) => `nav-link   ${isActive ? 'active':''}` }  to="/puentesdigitales" ><span className='active1' data-bs-toggle="collapse" data-bs-target="#navbarNav">  Empresa  </span>  </NavLink >
+                    <NavLink  className={ ({ isActive }) => `nav-link   ${isActive ? 'active':''}` } ><span className='active1' >  Empresa  </span>  </NavLink >
                 </li>
 
                 <div className="dropdown-content">
-                     <a> <NavLink   to="/puentesdigitales" ><span className='active2' data-bs-toggle="collapse" data-bs-target="#navbarNav">  Nosotros  </span>  </NavLink ></a>
-                     <a> <NavLink   to="/our-history" ><span className='active2' data-bs-toggle="collapse" data-bs-target="#navbarNav">  Nuestra Historia </span>  </NavLink ></a>
+                     <a> <NavLink   to="/puentesdigitales" ><span className='active2' >  Nosotros  </span>  </NavLink ></a>
+                     <a> <NavLink   to="/our-history" ><span className='active2' >  Nuestra Historia </span>  </NavLink ></a>
  
                   </div>
                  </div>
 
 
                 <li className="nav-item"     >
-                    <NavLink  className={ ({ isActive }) => `nav-link   ${isActive ? 'active':''}` }  to="/main-services"><span className='active1' data-bs-toggle="collapse" data-bs-target="#navbarNav"> Servicios </span> </NavLink >
+                    <NavLink  className={ ({ isActive }) => `nav-link   ${isActive ? 'active':''}` }  to="/main-services"><span className='active1'> Servicios </span> </NavLink >
                 </li>
 
 
                 <div className="dropdown">
                 <li className="nav-item">
-                    <NavLink  className={ ({ isActive }) => `nav-link   ${isActive ? 'active':''}` }  to="/nuestras-practicas" ><span className='active1' data-bs-toggle="collapse" data-bs-target="#navbarNav"> Nuestras Prácticas </span>  </NavLink >
+                    <NavLink  className={ ({ isActive }) => `nav-link   ${isActive ? 'active':''}` }  to="/nuestras-practicas" ><span className='active1' > Nuestras Prácticas </span>  </NavLink >
                 </li>
 
                 <div className="dropdown-content">
-                     <a> <NavLink   to="/ux-ui" ><span className='active2' data-bs-toggle="collapse" data-bs-target="#navbarNav">  UX/UI  </span>  </NavLink ></a>
-                     <a> <NavLink   to="/transformacion-digital" ><span className='active2' data-bs-toggle="collapse" data-bs-target="#navbarNav"> transformación Digital</span>  </NavLink ></a>
-                     <a> <NavLink   to="/software-arquitectura" ><span className='active2' data-bs-toggle="collapse" data-bs-target="#navbarNav"> Arquitectura de Software </span>  </NavLink ></a>
+                     <a> <NavLink   to="/ux-ui" ><span className='active2' >  UX/UI  </span>  </NavLink ></a>
+                     <a> <NavLink   to="/transformacion-digital" ><span className='active2' > transformación Digital</span>  </NavLink ></a>
+                     <a> <NavLink   to="/software-arquitectura" ><span className='active2' > Arquitectura de Software </span>  </NavLink ></a>
  
                   </div>
                  </div>
@@ -147,17 +131,17 @@ export const NavBar = () => {
 
                  <div className="dropdown">
                 <li className="nav-item">
-                    <NavLink  className={ ({ isActive }) => `nav-link   ${isActive ? 'active':''}` }  to="/services" ><span className='active1' data-bs-toggle="collapse" data-bs-target="#navbarNav"> Soluciones </span> </NavLink >
+                    <NavLink  className={ ({ isActive }) => `nav-link   ${isActive ? 'active':''}` }  to="/services" ><span className='active1' > Soluciones </span> </NavLink >
                 </li>               
   
   <div className="dropdown-content">
-    <a> <NavLink   to="/concesionarioweb" ><span className='active2' data-bs-toggle="collapse" data-bs-target="#navbarNav"> Cars Online </span> </NavLink ></a>
-    <a> <NavLink   to="/design" >         <span className='active2' data-bs-toggle="collapse" data-bs-target="#navbarNav">Desing  </span> </NavLink ></a>
-    <a> <NavLink   to="/webdesign" >      <span className='active2' data-bs-toggle="collapse" data-bs-target="#navbarNav">Web Desing</span> </NavLink ></a>
-    <a> <NavLink   to="/ecomerce" >       <span className='active2' data-bs-toggle="collapse" data-bs-target="#navbarNav">E-Comerce</span> </NavLink ></a>
-    <a> <NavLink   to="/appnative" >      <span className='active2' data-bs-toggle="collapse" data-bs-target="#navbarNav"> Mobile Delivery</span> </NavLink ></a>
-    <a> <NavLink   to="/shiftmagnamentsystem" ><span  className='active2' data-bs-toggle="collapse" data-bs-target="#navbarNav"> SGM  </span> </NavLink ></a>
-    <a> <NavLink   to="/magnamentsystem" ><span className='active2' data-bs-toggle="collapse" data-bs-target="#navbarNav"> Sist. de Gestion</span> </NavLink ></a>
+    <a> <NavLink   to="/concesionarioweb" >    <span className='active2' > Cars Online </span> </NavLink ></a>
+    <a> <NavLink   to="/design" >              <span className='active2' >Desing  </span> </NavLink ></a>
+    <a> <NavLink   to="/webdesign" >           <span className='active2' >Web Desing</span> </NavLink ></a>
+    <a> <NavLink   to="/ecomerce" >            <span className='active2' >E-Comerce</span> </NavLink ></a>
+    <a> <NavLink   to="/appnative" >           <span className='active2' > Mobile Delivery</span> </NavLink ></a>
+    <a> <NavLink   to="/shiftmagnamentsystem" ><span className='active2' > SGM  </span> </NavLink ></a>
+    <a> <NavLink   to="/magnamentsystem" >     <span className='active2' > Sist. de Gestion</span> </NavLink ></a>
   </div>
 </div>
 
@@ -175,7 +159,7 @@ export const NavBar = () => {
                     <NavLink  className="nav-link" to="/">Blog</NavLink >
                 </li> */}
                   <li className="nav-item">
-           <NavLink  className={ ({ isActive }) => `nav-link   ${isActive ? 'active':''}` }  to="/contact" ><span className='active1' data-bs-toggle="collapse" data-bs-target="#navbarNav"> Contacto </span> </NavLink >
+           <NavLink  className={ ({ isActive }) => `nav-link   ${isActive ? 'active':''}` }  to="/contact" ><span className='active1' > Contacto </span> </NavLink >
             </li>
             </ul>
             {/* <Link to="login" data-bs-toggle="modal" data-bs-target="#exampleModal"
